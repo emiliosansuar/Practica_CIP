4c52621f3846f6576344573de96aad48ae65e8b6
emiliosansuar <emilio.sansuar@gmail.com>
2024-04-17 17:07:17 +0200

    alu barebones

--- alu.vhd
+++ alu.vhd
@@ -1,1 +1,31 @@
 -- 3 registros para operaciones. 2 de valores y 1 de reultados 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity alu is
+  port
+  (
+    --entradas y salidas 
+    
+  );
+end entity alu;
+
+architecture arch_alu of alu is
+  
+    -- SIGNALS --------------------------------------
+  
+  
+  
+    -- Componentes -----------------------------------
+  
+  
+  
+  begin
+    -- Instanciacion de componentes  ----------------------------
+  
+    
+  
+    -- PROCESOS --------------------------------------------------
+  
+  end;
