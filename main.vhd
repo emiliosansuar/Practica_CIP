63c9cc86cfb2e49b4bd1285208b4dc3e225b0981
ralucaflorian06 <57660382+ralucaflorian06@users.noreply.github.com>
2024-04-24 11:54:06 +0200

    cambio pequeño

--- main.vhd
+++ main.vhd
@@ -16,10 +16,7 @@ end entity Practica_CIP;
 
 architecture arch_Practica_CIP of Practica_CIP is
   
-  type memory_type is array(0 to 9) of std_logic_vector(31 downto 0); -- AUMENTAR LUEGO HASTA 64
-  signal memory_type : memoria := ("00000000000000000000000000000000", 
   
-  );
   -- SIGNALS --------------------------------------
 
 begin
--- memoria.vhd
+++ memoria.vhd
@@ -1,1 +1,37 @@
 -- Códgio de memoria definido en el enunciado de la práctica.
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity memoria is
+  port
+  (
+    --entradas y salidas 
+    
+  );
+end entity memoria;
+
+architecture arch_memoria of memoria is
+    type memory_type is array(0 to 9) of std_logic_vector(31 downto 0); -- AUMENTAR LUEGO HASTA 64
+    signal memory : memoria_type := (
+        others => (others => '0') -- Inicializa todas las direcciones con ceros
+    );
+
+
+
+    -- SIGNALS --------------------------------------
+  
+  
+  
+    -- Componentes -----------------------------------
+  
+  
+  
+  begin
+    -- Instanciacion de componentes  ----------------------------
+  
+    
+  
+    -- PROCESOS --------------------------------------------------
+  
+  end;
